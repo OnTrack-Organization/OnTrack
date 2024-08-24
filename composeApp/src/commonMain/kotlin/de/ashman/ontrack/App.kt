1a192b29022250070fa9c2a12f599a4d2f4181aa
Ashkan Haghighi Fashi <Ashkan.HaghighiFashi@hermesworld.com>
2024-08-24 17:56:37 +0200

    remove apple login for now

--- composeApp/src/commonMain/kotlin/de/ashman/ontrack/App.kt
+++ composeApp/src/commonMain/kotlin/de/ashman/ontrack/App.kt
@@ -1,13 +1,11 @@
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -21,16 +19,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.mmk.kmpauth.firebase.apple.AppleButtonUiContainer
-import com.mmk.kmpauth.firebase.github.GithubButtonUiContainer
 import com.mmk.kmpauth.firebase.google.GoogleButtonUiContainerFirebase
 import com.mmk.kmpauth.google.GoogleAuthCredentials
 import com.mmk.kmpauth.google.GoogleAuthProvider
-import com.mmk.kmpauth.google.GoogleButtonUiContainer
-import com.mmk.kmpauth.uihelper.apple.AppleSignInButton
-import com.mmk.kmpauth.uihelper.apple.AppleSignInButtonIconOnly
 import com.mmk.kmpauth.uihelper.google.GoogleSignInButton
-import com.mmk.kmpauth.uihelper.google.GoogleSignInButtonIconOnly
 import dev.gitlive.firebase.auth.FirebaseUser
 
 @Composable
@@ -55,51 +47,21 @@ fun App() {
                     println("Error Result: ${result.exceptionOrNull()?.message}")
                 }
             }
+
             Text(
                 text = signedInUserName,
                 style = MaterialTheme.typography.body1,
                 textAlign = TextAlign.Start,
             )
 
-            // Google Sign-In with Custom Button and authentication without Firebase
-            GoogleButtonUiContainer(onGoogleSignInResult = { googleUser ->
-                val idToken = googleUser?.idToken // Send this idToken to your backend to verify
-                signedInUserName = googleUser?.displayName ?: "Null User"
-            }) {
-                Button(onClick = { this.onClick() }) { Text("Google Sign-In(Custom Design)") }
-            }
-
-            // Apple Sign-In with Custom Button and authentication with Firebase
-            AppleButtonUiContainer(onResult = onFirebaseResult) {
-                Button(onClick = { this.onClick() }) { Text("Apple Sign-In (Custom Design)") }
-            }
-
-            // Github Sign-In with Custom Button and authentication with Firebase
-            GithubButtonUiContainer(onResult = onFirebaseResult) {
-                Button(onClick = { this.onClick() }) { Text("Github Sign-In (Custom Design)") }
-            }
-
-            // ************************** UiHelper Text Buttons *************
             Divider(
                 modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
             )
-            AuthUiHelperButtonsAndFirebaseAuth(
+            GoogleSignInButton(
                 modifier = Modifier.width(IntrinsicSize.Max),
-                onFirebaseResult = onFirebaseResult,
-            )
-
-            // ************************** UiHelper IconOnly Buttons *************
-            Divider(
-                modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-            )
-            IconOnlyButtonsAndFirebaseAuth(
-                modifier = Modifier.fillMaxWidth(),
                 onFirebaseResult = onFirebaseResult,
             )
         }
@@ -107,7 +69,7 @@ fun App() {
 }
 
 @Composable
-fun AuthUiHelperButtonsAndFirebaseAuth(
+fun GoogleSignInButton(
     modifier: Modifier = Modifier,
     onFirebaseResult: (Result<FirebaseUser?>) -> Unit,
 ) {
@@ -115,18 +77,17 @@ fun AuthUiHelperButtonsAndFirebaseAuth(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(10.dp),
     ) {
-        // Google Sign-In Button and authentication with Firebase
         GoogleButtonUiContainerFirebase(onResult = onFirebaseResult) {
             GoogleSignInButton(
                 modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .height(44.dp),
+                    Modifier
+                        .fillMaxWidth()
+                        .height(44.dp),
                 fontSize = 19.sp,
-            ) { this.onClick() }
+            ) { onClick() }
         }
 
-        // Apple Sign-In Button and authentication with Firebase
+        /*// Apple Sign-In Button and authentication with Firebase
         AppleButtonUiContainer(onResult = onFirebaseResult) {
             AppleSignInButton(
                 modifier =
@@ -134,28 +95,7 @@ fun AuthUiHelperButtonsAndFirebaseAuth(
                     .fillMaxWidth()
                     .height(44.dp),
             ) { this.onClick() }
-        }
-    }
-}
-
-@Composable
-fun IconOnlyButtonsAndFirebaseAuth(
-    modifier: Modifier = Modifier,
-    onFirebaseResult: (Result<FirebaseUser?>) -> Unit,
-) {
-    Row(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally),
-    ) {
-        // Google Sign-In IconOnly Button and authentication with Firebase
-        GoogleButtonUiContainerFirebase(onResult = onFirebaseResult) {
-            GoogleSignInButtonIconOnly(onClick = { this.onClick() })
-        }
-
-        // Apple Sign-In IconOnly Button and authentication with Firebase
-        AppleButtonUiContainer(onResult = onFirebaseResult) {
-            AppleSignInButtonIconOnly(onClick = { this.onClick() })
-        }
+        }*/
     }
 }
 
