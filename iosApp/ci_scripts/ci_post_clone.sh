027c69cb3a1fe8737d50c93f7b5576e1decab065
Ashkan Haghighi Fashi <Ashkan.HaghighiFashi@hermesworld.com>
2025-02-28 00:29:40 +0100

    Make clone script read and write

--- iosApp/ci_scripts/ci_post_clone.sh
+++ iosApp/ci_scripts/ci_post_clone.sh
@@ -6,13 +6,6 @@
 #  Created by Ashkan Haghighi Fashi on 27.02.25.
 #  Copyright © 2025 orgName. All rights reserved.
 
-echo "Setting JAVA_HOME for Xcode Cloud"
-export JAVA_HOME=/Users/ashkanhaghighifashi/Library/Java/JavaVirtualMachines/corretto-21.0.6/Contents/Home
-echo "JAVA_HOME set to $JAVA_HOME"
-
-#echo "Ensuring GoogleService-Info.plist exists"
-# Create the correct directory if it doesn't exist
-#mkdir -p /Volumes/Desktop/iosApp/iosApp/
-# Copy the file to the expected location
-#cp /Users/ashkanhaghighifashi/StudioProjects/OnTrack/iosApp/iosApp/GoogleService-Info.plist /Volumes/Desktop/iosApp/iosApp/
-#echo "GoogleService-Info.plist copied successfully"
+#echo "Setting JAVA_HOME for Xcode Cloud"
+#export JAVA_HOME=/Users/ashkanhaghighifashi/Library/Java/JavaVirtualMachines/corretto-21.0.6/Contents/Home
+#echo "JAVA_HOME set to $JAVA_HOME"
