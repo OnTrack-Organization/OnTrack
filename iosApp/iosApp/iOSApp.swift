483b86211e2483df5a74256637c10b3f8f6020a3
Ashkan Haghighi Fashi <Ashkan.HaghighiFashi@hermesworld.com>
2025-02-28 09:57:33 +0100

    Fix? iosapp swift and contentview swift location

---
+++ iosApp/iosApp/iOSApp.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+import GoogleSignIn
+import FirebaseCore
+import FirebaseMessaging
+import ComposeApp
+
+@main
+struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+   var body: some Scene {
+      WindowGroup {
+            ContentView()
+              .ignoresSafeArea()
+              .onOpenURL(perform: { url in
+                GIDSignIn.sharedInstance.handle(url)
+            })
+              .onAppear {
+                GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+                  // Check if `user` exists; otherwise, do something with `error`
+                }
+              }
+      }
+   }
+}
+
+// Handles Urls and if its google stuff it does stuff
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    override init() {
+        ModuleKt.doInitKoin(appDeclaration: {_ in})
+        
+        FirebaseApp.configure()
+        
+        NotifierManager.shared.initialize(configuration: NotificationPlatformConfigurationIos(
+                    showPushNotification: true,
+                    askNotificationPermissionOnStart: true,
+                    notificationSoundName: "default"
+        )
+              )
+        }
+    
+    func application(
+      _ app: UIApplication,
+      open url: URL, 
+      options: [UIApplication.OpenURLOptionsKey : Any] = [:],
+      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) -> Bool {
+        Messaging.messaging().apnsToken = deviceToken
+        
+      var handled: Bool
+
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
+
+      return false
+    }
+}
