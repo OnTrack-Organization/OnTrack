c6cc9e972cae83288f545a1c84232cb8b9139cba
Ashkan Haghighi Fashi <Ashkan.HaghighiFashi@hermesworld.com>
2025-03-11 19:46:23 +0100

    fix strings

--- composeApp/src/iosMain/kotlin/de/ashman/ontrack/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/de/ashman/ontrack/MainViewController.kt
@@ -1,7 +1,6 @@
 package de.ashman.ontrack
 
 import androidx.compose.ui.window.ComposeUIViewController
-import de.ashman.ontrack.di.initKoin
 
 fun MainViewController() =
     ComposeUIViewController {
